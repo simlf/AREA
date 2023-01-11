0b44244c45ed818364475c2ed2ee6c123902d311
simlf <simon.le-fourn@epitech.eu>
2023-01-11 01:28:59 +0100

    :sparkles: Add users model

--- nestjs-mongodb-crud/src/app.module.ts
+++ nestjs-mongodb-crud/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ nestjs-mongodb-crud/src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return new this.userModel(createUserDto).save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  findOne(username: string) {
+    return this.userModel.findOne({ username });
+  }
+
+  update(username: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.updateOne({ username }, { $set: updateUserDto })
+  }
+
+  remove(username: string) {
+    return this.userModel.deleteOne({ username })
+  }
+}
