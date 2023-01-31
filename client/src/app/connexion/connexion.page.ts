38703b6c0583efb0a6456368db04e381c18c2403
Louis Maestre <louismaestre@MacBook-Air-de-Louis.local>
2023-01-31 16:41:38 +0100

    :sparkles: connexion and inscription with back end

--- client/src/app/connexion/connexion.page.ts
+++ client/src/app/connexion/connexion.page.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { distinctUntilChanged, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { login } from '../models/login.model';
 
 @Component({
   selector: 'app-connexion',
@@ -20,7 +22,38 @@ export class ConnexionPage implements OnInit {
       distinctUntilChanged()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) { }
+
+  auth_token_login = "";
+  // auth_token_whoami = "";
+  // headersWhoami = new HttpHeaders({
+  //   'Content-Type': 'application/json',
+  //   'Authorization': `Bearer ${this.auth_token_whoami}`
+  // });
+
+  headersLogin = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${this.auth_token_login}`
+  });
+
+  bodyLogin: login = {
+    email: '',
+    password: ''
+  };
+
+  rootURL = 'http://localhost:8080/api/auth';
+
+  // getWhoami() {
+  //   this.http.get(this.rootURL + "/whoami", { headers: this.headersWhoami })
+  //     .subscribe((res) => { console.log(res); });
+  // }
+
+  postLogin() {
+    console.log(this.bodyLogin.email);
+    console.log(this.bodyLogin.password);
+    this.http.post(this.rootURL + "/login", this.bodyLogin, { headers: this.headersLogin })
+      .subscribe((res) => { console.log(res); });
+  }
 
   ngOnInit(): void {
     this.breakpoint$.subscribe(() =>
--- client/src/app/inscription/inscription.page.ts
+++ client/src/app/inscription/inscription.page.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { distinctUntilChanged, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { register } from '../models/register.model';
 
 @Component({
   selector: 'app-inscription',
@@ -12,6 +14,33 @@ export class InscriptionPage implements OnInit {
 
   Breakpoints = Breakpoints;
   currentBreakpoint:string = '';
+  confirmPassword:string = '';
+  auth_token_login = '';
+  valid:boolean= true;
+
+  headersLogin = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${this.auth_token_login}`
+  });
+
+  bodyRegister: register = {
+    username: '',
+    password: '',
+    email: ''
+  };
+  
+  rootURL = 'http://localhost:8080/api/auth';
+
+  postRegister() {
+    if (this.bodyRegister.password !== this.confirmPassword) {
+      this.valid = false;
+      return;
+    }
+    this.http.post(this.rootURL + "/register", this.bodyRegister)
+      .subscribe((res) => { console.log(res); });
+    this.valid = true;
+    return;
+  }
 
   readonly breakpoint$ = this.breakpointObserver
     .observe([Breakpoints.Web, Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait])
@@ -20,7 +49,7 @@ export class InscriptionPage implements OnInit {
       distinctUntilChanged()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.breakpoint$.subscribe(() =>
