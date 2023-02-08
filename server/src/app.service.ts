84f8bc16b0f2c2fc6625a803c9f4b733d3d906c1
tistech0 <matthis.lesur@epitech.eu>
2023-02-08 19:11:16 +0100

    :building_construction: remove api from base url and form about.json

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Request } from '@nestjs/common';
+import { Response } from 'express';
 @Injectable()
 export class AppService {
   getHello(): string {
@@ -7,9 +7,63 @@ export class AppService {
   }
 }
 
+export interface action {
+  name: string;
+  description: string;
+}
+
+export interface reaction {
+  name: string;
+  description: string;
+}
+
+export interface Service {
+  name: string;
+  actions: action[];
+  reactions: reaction[];
+}
+
+export interface Client {
+  host: string;
+}
+
+export interface Server {
+  currentTimeServer: string;
+  services: Service[];
+}
+export interface About {
+  client: Client;
+  server: Server; 
+}
+
 @Injectable()
 export class AboutService {
-  getAbout(): string {
-    return 'Hello World!';
+  getAbout(@Request() Req): About {
+    const about: About = {
+      client: {
+        host: Req.ip,
+      },
+      server: {
+        currentTimeServer: new Date().getTime().toString(),
+        services: [
+          {
+            name: 'Discord',
+            actions: [
+              {
+                name: 'Send message',
+                description: 'Send a message to a channel',
+              },
+            ],
+            reactions: [
+              {
+                name: 'Send message',
+                description: 'Send a message to a channel',
+              },
+            ],
+          },
+        ],
+      }
+    };
+    return about;
   }
 }
