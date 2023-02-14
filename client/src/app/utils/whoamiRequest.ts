b3def6dfc775805da744e50ffa6e5328bf1220ec
tistech0 <matthis.lesur@epitech.eu>
2023-02-14 15:33:50 +0100

    :truck: add class for request whoami

--- client/src/app/utils/whoamiRequest.ts
+++ client/src/app/utils/whoamiRequest.ts
@@ -2,19 +2,6 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import * as _ from 'lodash';
 
 /**
- * A class to store the response data of a login request.
- */
-export class responseLogin {
-    /**
-     * Constructor for responseLogin class.
-     * @param email The email address of the user.
-     * @param accessToken The access token for the user.
-     * @param id The id of the user.
-     */
-    constructor(public email: string, public accessToken: string, public id: string) { }
-}
-
-/**
  * A class to handle the login request and store the login response data.
  */
 export class whoamiRequest {
@@ -36,12 +23,12 @@ export class whoamiRequest {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + localStorage.getItem('auth_token'),
         });
-        const response = await this.http.get('http://localhost:8080/auth/whoami', { headers: headersBearrer }).toPromise();
-        console.log(response);
-        if (JSON.stringify(response).includes("400"))
-            console.log("400");
-        else
-            console.log("200");
-        return false;
+        const response = await this.http.get('http://localhost:8080/auth/whoami', { headers: headersBearrer }).toPromise()
+        .then((data) => {
+            return true;
+        }).catch((error) => {
+            return false;
+        });
+        return response;
     }
 }
