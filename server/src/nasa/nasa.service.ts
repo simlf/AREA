f448d174ff5e3114d28db7bc102507e159a81926
Louis Maestre <louis.maestre@epitech.eu>
2023-02-15 15:17:38 +0100

    :sparkles: delete debug

--- server/src/nasa/nasa.service.ts
+++ server/src/nasa/nasa.service.ts
@@ -14,13 +14,10 @@ export class NasaService {
         var return_value = { 
             "url"                   :  null,                  
         }
-        console.log(base_url1);
-        console.log(process.env.NASA_API);
         const url_tmp = `${base_url1}${process.env.NASA_API}`;
         try {
             const { data }  = await firstValueFrom(this.httpService.get(url_tmp))
             const level = data.date;
-            console.log(data.url);
             return_value.url = data.url;
         } catch (error) {            
             return {"Error" : error.code, "Message" : error.message}
