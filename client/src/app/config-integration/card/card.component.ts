e8bcb0db23070ee6ef83241e02ef79c21b8c83e7
tistech0 <matthis.lesur@epitech.eu>
2023-03-02 15:23:45 +0100

    :sparkles: add description for card

--- client/src/app/config-integration/card/card.component.ts
+++ client/src/app/config-integration/card/card.component.ts
@@ -8,6 +8,7 @@ import { ConfigIntegrationPage } from '../config-integration.page';
 import { workflowRequest, responseWorkflows } from 'src/app/utils/workflowRequest';
 import { CardComponent } from 'src/app/integration/card/card.component';
 import { ActivatedRoute } from '@angular/router';
+import { service } from 'src/app/models/service.model';
 
 @Component({
   selector: 'app-card',
@@ -44,11 +45,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class integrationComponent implements OnInit {
 
-  integration: integration[] = [];
+  public integration: integration[] = [];
   Breakpoints = Breakpoints;
   currentBreakpoint: string = '';
   rootURL = 'http://localhost:8080/';
   id: string | null = null;
+  serviceDescription: service[] = [];
 
   readonly breakpoint$ = this.breakpointObserver
     .observe([Breakpoints.Web, Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait])
@@ -57,13 +59,23 @@ export class integrationComponent implements OnInit {
       distinctUntilChanged()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient, private route: ActivatedRoute) { };
+  constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient, private route: ActivatedRoute) {
+    this.serviceDescription = [
+      { name: 'spotify', description: 'Spotify est un service de musique en streaming qui vous permet d\'écouter de la musique sur votre ordinateur, votre téléphone ou votre tablette. Il vous offre un accès à des millions de titres.'},
+      { name: 'nasa', description: "La NASA propose une API qui permet de récupérer l'image astronomique du jour (APOD) ainsi que les données associées."},
+      { name: "meteo", description: "Un service météo peut être utilisé pour déclencher une action lorsque certaines conditions météorologiques sont atteintes."},
+      { name: "leagues", description: "L'API de League of Legends permet de récupérer des informations sur les joueurs, les équipes et les matchs."},
+      { name: "reddit", description: "Reddit est un site web communautaire qui permet à ses utilisateurs de poster des liens vers des articles, des images, des vidéos, etc. et de commenter les publications des autres utilisateurs."},
+      ];
+    };
 
   async getWorkflow() {
     const bodyRequest = { "workflowId": this.id };
     const workflow: workflowRequest = new workflowRequest(this.http);
     const result = await workflow.postData(this.rootURL + "workflowsDb/getWorkflow", bodyRequest, responseWorkflows);
-    this.integration = [{ name: result.actionName, description: '', img: result.img, logo: result.logo, connect: false}, { name: result.reactionName, description: '', img: result.img, logo: result.logo, connect: false}];
+    const actiondesc = this.serviceDescription.find(service => service.name === result.actionName)?.description || '';
+    const reactiondesc = this.serviceDescription.find(service => service.name === result.reactionName)?.description || '';
+    this.integration = [{ name: result.actionName, description: actiondesc, img: result.img, logo: result.logo, connect: false}, { name: result.reactionName, description: reactiondesc, img: result.img, logo: result.logo, connect: false}];
     console.log(result);
   };
 
